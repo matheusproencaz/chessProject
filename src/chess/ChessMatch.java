f233a48c6520c6609d5366df62bdf8030728dcc0
Matheus Proenca <matheusproencaz@gmail.com>
2022-01-26 10:36:36 -0300

    Movimentos possíveis de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,10 +43,12 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
-	
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece!");
 		}
 	}
 	
